132f5f5c74e4701ea611ae7b777a9fd30296e07b
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-05-29 05:52:12 +0600

    added sheet 2 some sums problem solution

--- Practice Sheet 2(loop) Problems in CodeForces/U_Some_Sums.cpp
+++ Practice Sheet 2(loop) Problems in CodeForces/U_Some_Sums.cpp
@@ -1,6 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int digitCount(int n){
+    int sum_of_dg = 0;
+    while (n > 0)
+    {
+        int digit = n % 10;
+        sum_of_dg += digit;
+        n /= 10;
+    }
     
+    return sum_of_dg;
+}
+
+int main()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    int sum_of_num = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+
+       if(digitCount(i) >= a && digitCount(i) <= b){
+        sum_of_num += i;
+       }
+       
+        
+    }
+    
+    cout << sum_of_num << endl;
     return 0;
 }
