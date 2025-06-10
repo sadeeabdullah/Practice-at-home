f6068d0f7895f238d54176b53afda2bb6749eb76
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-10 11:16:25 +0600

    adding initial steps

---
+++ contest 2 Practice problem in code forces/G_Construct_the_Sum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        long long int n, s;
+        cin >> n >> s;
+
+        for (int i = n; i < 0; i--)
+        {
+           cout << i
+        }
+        
+    }
+    return 0;
+}
