71f4266654635b79ef807476cbe6052f1135d0a3
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-16 07:05:18 +0600

    added frequency array problem solution approach

---
+++ sheet 3 practice problem in codeforces/V_Frequency_Array.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+    //taking frequency array as the size of m
+    int freq[m] ={0}; // set 0 to every value in frequency array
+    for (int i = 0; i <n; i++)
+    {
+        freq[a[i]]++;
+    }
+
+    //showing the count of number repeating in array a using frequency array
+    for (int i = 1; i <= m; i++)
+    {
+        cout << freq[i] << endl;
+    }
+    
+    
+    return 0;
+}
