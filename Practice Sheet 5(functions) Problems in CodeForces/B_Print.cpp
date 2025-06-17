bf5f91b82d4f114748d16e0dec728dbf6b31bd16
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-17 06:30:56 +0600

    added print prblem solution with function

---
+++ Practice Sheet 5(functions) Problems in CodeForces/B_Print.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int prnt(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    prnt(n);
+    return 0;
+}
