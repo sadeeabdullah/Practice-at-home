f5a1c003a86d94fcea6aa56de380c99b61e8d0b9
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-19 13:51:50 +0600

    swapping with matrix problem solution accepted

--- Practice Sheet 5(functions) Problems in CodeForces/I_Swapping_With_Matrix.cpp
+++ Practice Sheet 5(functions) Problems in CodeForces/I_Swapping_With_Matrix.cpp
@@ -4,21 +4,28 @@ using namespace std;
 //swap function for specific row and column
 void swapping(int n,int x, int y, int **a)
 {
+    //swap x row with y row
     for (int i = 0; i < n; i++)
     {
-        swap(a[i][x - 1], a[i][y -1]);
+        swap(a[x - 1][i], a[y - 1][i]);
     }
+    //swap x row with y row
     for (int i = 0; i < n; i++)
     {
-        swap(a[x -1][i], a[y -1][i]);
+        swap(a[i][x - 1], a[i][y - 1]);
     }
 }
 int main()
 {
     int n, x, y;
     cin >> n >> x >> y;
-    int a[n][n];
 
+    int** a = new int*[n];
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = new int[n];
+    }
+    
     //taking the array input
     for (int i = 0; i < n; i++)
     {
@@ -30,6 +37,17 @@ int main()
 
 
     swapping(n, x, y, a);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+        
+    }
+    
     
     return 0;
 }
