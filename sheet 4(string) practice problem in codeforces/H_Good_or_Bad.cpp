925e75d2036a71de6910641021a29d830e031424
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-23 12:23:47 +0600

    added h good or bad problem solution

---
+++ sheet 4(string) practice problem in codeforces/H_Good_or_Bad.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        string s;
+        cin >> s;
+        int flag = 0;
+        for (int i = 0; i < s.size() - 2; i++)
+        {
+            string tmp = "";
+            tmp += s[i] ;
+            tmp += s[i + 1] ;
+            tmp += s[i + 2];
+
+            if (tmp == "101" || tmp == "010")
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        (flag == 0) ? cout << "Bad" << endl
+                    : cout << "Good" << endl;
+    }
+    return 0;
+}
