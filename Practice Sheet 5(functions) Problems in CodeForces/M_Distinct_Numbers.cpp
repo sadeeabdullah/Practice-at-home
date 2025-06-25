5d0cafb40aebade12ca056a272583c96b7a5de40
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-25 17:56:48 +0600

    added more optimal solution for counting the distinct number on a vector or array using unordered_set to minimize the complexity to O(N)

--- Practice Sheet 5(functions) Problems in CodeForces/M_Distinct_Numbers.cpp
+++ Practice Sheet 5(functions) Problems in CodeForces/M_Distinct_Numbers.cpp
@@ -4,16 +4,24 @@ using namespace std;
 // distinct number count function
 int dstnt(int n, vector<int> &a)
 {
-    int cnt = 0;
-    sort(a.begin(), a.end());
+    // int cnt = 0;
+    // sort(a.begin(), a.end()); // time complexity O(N log N);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if(a[i] != a[i + 1])
+    //     {
+    //         cnt++;
+    //     }
+    // }
+    // return cnt;
+
+    //more optimal? yes here it is: time complexity will be O(N)
+    unordered_set<int> s(n);
     for (int i = 0; i < n; i++)
     {
-        if(a[i] != a[i + 1])
-        {
-            cnt++;
-        }
+        s.insert(a[i]); // it will insert the unique number from the vector a and  ignore the duplicates
     }
-    return cnt;
+    return s.size();
 }
 int main()
 {
