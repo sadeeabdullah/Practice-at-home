ec386424c98f88670358e365ca9fd3f7e61ca99e
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-27 07:23:21 +0600

    added approach to solve string function problem but WA on test 2

--- sheet 4(string) practice problem in codeforces/L_String_Functions.cpp
+++ sheet 4(string) practice problem in codeforces/L_String_Functions.cpp
@@ -1,45 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+int main()
+{
 
     int n, q;
     cin >> n >> q;
     string s;
     cin >> s;
 
-    while (q--) {
+    while (q--)
+    {
         string cmd;
         cin >> cmd;
 
-        if (cmd == "pop_back") {
+        if (cmd == "pop_back")
+        {
             s.pop_back();
-        } else if (cmd == "front") {
-            cout << s.front() << '\n';
-        } else if (cmd == "back") {
-            cout << s.back() << '\n';
-        } else if (cmd == "push_back") {
+        }
+        else if (cmd == "front")
+        {
+            cout << s.front() << endl;
+        }
+        else if (cmd == "back")
+        {
+            cout << s.back() << endl;
+        }
+        else if (cmd == "sort")
+        {
+            int l, r;
+            cin >> l >> r;
+            if (l >= 1 && r <= s.size() && l <= r)
+                sort(s.begin() + l - 1, s.begin() + r);
+        }
+        else if (cmd == "reverse")
+        {
+            int l, r;
+            cin >> l >> r;
+            if (l >= 1 && r <= s.size() && l <= r)
+                reverse(s.begin() + l - 1, s.begin() + r);
+        }
+        else if (cmd == "print")
+        {
+            int pos;
+            cin >> pos;
+            pos--;
+            if (pos >= 0 && pos < s.size())
+                cout << s[pos] << endl;
+        }
+        else if (cmd == "substr")
+        {
+            int l, r;
+            cin >> l >> r;
+            if (l >= 1 && r <= s.size() && l <= r)
+                cout << s.substr(l - 1, r - l + 1) << endl;
+        }
+        else if (cmd == "push_back")
+        {
             char x;
             cin >> x;
             s.push_back(x);
-        } else if (cmd == "print") {
-            int pos;
-            cin >> pos;
-            cout << s[pos - 1] << '\n';
-        } else if (cmd == "substr") {
-            int l, r;
-            cin >> l >> r;
-            cout << s.substr(l - 1, r - l + 1) << '\n';
-        } else if (cmd == "sort") {
-            int l, r;
-            cin >> l >> r;
-            sort(s.begin() + l - 1, s.begin() + r);
-        } else if (cmd == "reverse") {
-            int l, r;
-            cin >> l >> r;
-            reverse(s.begin() + l - 1, s.begin() + r);
         }
     }
 
